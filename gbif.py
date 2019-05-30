573c692af00160ed6c8f77b7b633c94e7e9a3e90
Paul <p.kiddle@nhm.ac.uk>
2019-05-30 11:48:12 +0100

    Add gbif cron job

--- gbif.py
+++ gbif.py
@@ -4,13 +4,17 @@ from app.gbif import pull_tags
 
 def update_tags():
   """ Updates taxonomy tags from gbif backbone and deletes unused ones """
+  print('Updating tags:')
   for scan in Scan.query.filter(Scan.gbif_id).all():
     tags = [
       db.session.merge(tag) for tag in pull_tags(scan.gbif_id)
     ]
     scan.taxonomy = tags
+    print(' - ', scan.scientific_name)
 
+  print('Deleting tags:')
   for tax in Taxonomy.query.filter(db.not_(Taxonomy.scans.any())):
+    print(' - ', tax.name)
     db.session.delete(tax)
   db.session.commit()
 
