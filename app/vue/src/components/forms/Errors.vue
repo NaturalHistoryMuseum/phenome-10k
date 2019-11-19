ba308a419762307bd7a6c20eae6729bd92fcf7a4
Paul Kiddle <p.kiddle@nhm.ac.uk>
2019-11-19 17:46:02 +0000

    Add better error handling for json responses that are html pages

--- app/vue/src/components/forms/Errors.vue
+++ app/vue/src/components/forms/Errors.vue
@@ -1,16 +1,40 @@
 <template>
-  <ul class="Errors">
+  <ul :class="classes" v-if="!modal">
     <li v-for="error in errors" :key="error">
       {{ error.message || error }}
     </li>
   </ul>
+  <div :class="classes" v-else @click.self="dismiss" v-show="!dismissed">
+    <button class="Errors__close" @click="dismiss">Close</button>
+    <iframe class="Errors__iframe" v-for="error in errors" :key="error" :srcdoc="error.message || error" />
+  </div>
 </template>
 
 <style>
 .Errors {
-  list-style: none;
   margin: 0;
   padding: 0;
+}
+
+.Errors--list {
+  list-style: none;
+}
+
+.Errors--modal {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  left: 0;
+  height: 100%;
+  background: #0009;
+  overflow: auto;
+  display: grid;
+  padding: 5%;
+  grid-template-rows: max-content;
+}
+
+.Errors__close {
+  justify-self: end;
 }
 
 .Errors li {
@@ -21,6 +45,33 @@
 <script>
 export default {
   name: 'FormErrors',
-  props: ['errors']
+  props: ['errors'],
+  data: () => {
+    return {
+      dismissed: false
+    }
+  },
+  methods: {
+    dismiss(){
+      this.dismissed = true;
+    }
+  },
+  computed: {
+    modal(){
+      return this.errors.some(e => typeof e === 'string' && e[0] === '<');
+    },
+    classes(){
+      return {
+        'Errors': true,
+        'Errors--modal': this.modal,
+        'Errors--list': !this.modal,
+      }
+    }
+  },
+  watch: {
+    errors(){
+      this.dismissed = false;
+    }
+  }
 }
 </script>
