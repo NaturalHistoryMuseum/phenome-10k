73a6d6bde79e962d5d3e572621f4213933358209
Alice Butcher <alycejenni@gmail.com>
2024-03-12 11:36:02 +0000

    style: run pre-commit on ansible files

--- ansible/get-token.py
+++ ansible/get-token.py
@@ -11,34 +11,39 @@ sys.stderr.write('Vault LDAP username: ')
 user = input()
 pswd = getpass.getpass('Vault LDAP password: ')
 
-vault_url = 'https://man-vault-2.nhm.ac.uk:8200/v1/auth/ldap/login/' + user # Set destination URL here
-post_fields = {'password': pswd}     # Set POST fields here
+vault_url = (
+    'https://man-vault-2.nhm.ac.uk:8200/v1/auth/ldap/login/' + user
+)  # Set destination URL here
+post_fields = {'password': pswd}  # Set POST fields here
 
 request = Request(vault_url, json.dumps(post_fields).encode())
 try:
-  resp = urlopen(request).read().decode()
-  auth = json.loads(resp)['auth']
-  token = auth['client_token']
-  duration = auth['lease_duration']
-  unit = 'seconds'
-  if duration >= 60:
-  	duration = duration / 60
-  	unit = 'minutes'
+    resp = urlopen(request).read().decode()
+    auth = json.loads(resp)['auth']
+    token = auth['client_token']
+    duration = auth['lease_duration']
+    unit = 'seconds'
+    if duration >= 60:
+        duration = duration / 60
+        unit = 'minutes'
 
-  	if duration >= 60:
-  		duration = duration / 60
-  		unit = 'hours'
+        if duration >= 60:
+            duration = duration / 60
+            unit = 'hours'
 
-  		if duration >= 24:
-  			duration = duration / 24
-  			unit = 'days'
-  print(f'Got token OK. It\'s valid for {duration:g} {unit}.', file=sys.stderr)
-  print(token)
+            if duration >= 24:
+                duration = duration / 24
+                unit = 'days'
+    print(f'Got token OK. It\'s valid for {duration:g} {unit}.', file=sys.stderr)
+    print(token)
 except HTTPError as e:
-  print(f'Failed to get a token, sorry. Vault says:', file=sys.stderr)
-  resp = e.read().decode()
-  errors = json.loads(resp)['errors']
-  sys.exit('\n'.join(errors))
+    print(f'Failed to get a token, sorry. Vault says:', file=sys.stderr)
+    resp = e.read().decode()
+    errors = json.loads(resp)['errors']
+    sys.exit('\n'.join(errors))
 except URLError as e:
-  print(f'Could not connect to vault server. Are you on the museum network?', file=sys.stderr)
-  sys.exit(e)
+    print(
+        f'Could not connect to vault server. Are you on the museum network?',
+        file=sys.stderr,
+    )
+    sys.exit(e)
