ec582aa070ec6685790c5162cb54098722d5ccf9
Alice Butcher <alycejenni@gmail.com>
2024-03-12 11:47:28 +0000

    style: unignore and reformat build scripts

--- web/build/watch.js
+++ web/build/watch.js
@@ -1,18 +1,17 @@
-const {
-  compiler,
-  clear
-} = require('.');
+const { compiler, clear } = require('.');
 
 clear().then(() => {
-  compiler.watch({},   (err, stats) => {
+  compiler.watch({}, (err, stats) => {
     if (err) {
       console.error(err);
       return;
     }
 
-    console.log(stats.toString({
-      chunks: false,  // Makes the build much quieter
-      colors: true    // Shows colors in the console
-    }));
+    console.log(
+      stats.toString({
+        chunks: false, // Makes the build much quieter
+        colors: true, // Shows colors in the console
+      }),
+    );
   });
 });
--- web/build/webpack.client.config.js
+++ web/build/webpack.client.config.js
@@ -1,14 +1,14 @@
-const path = require('path')
-const merge = require('webpack-merge')
-const baseConfig = require('./webpack.base.config.js')
-const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
+const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.config.js');
+const VueSSRClientPlugin = require('vue-server-renderer/client-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(baseConfig, {
   entry: './node/src/entry-client.js',
   output: {
     path: path.resolve('../static/dist'),
-    publicPath: '/static/dist/'
+    publicPath: '/static/dist/',
   },
   plugins: [
     // This plugins generates `vue-ssr-client-manifest.json` in the
@@ -24,19 +24,19 @@ module.exports = merge(baseConfig, {
           MiniCssExtractPlugin.loader,
           {
             loader: 'css-loader',
-            options: {modules: true, localIdentName: '[name]__[local]'}
+            options: { modules: true, localIdentName: '[name]__[local]' },
           },
           'sass-loader',
-        ]
-      }
-    ]
+        ],
+      },
+    ],
   },
   // Important: this splits the webpack runtime into a leading chunk
   // so that async chunks can be injected right after it.
   // this also enables better caching for your app/vendor code.
   optimization: {
     runtimeChunk: {
-      name: 'manifest'
-    }
-  }
-})
+      name: 'manifest',
+    },
+  },
+});
